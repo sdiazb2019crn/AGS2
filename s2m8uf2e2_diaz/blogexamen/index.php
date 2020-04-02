9fc3239333f19ce540047b5009befbec488dee4a
sdiazb2019crn <56316938+sdiazb2019crn@users.noreply.github.com>
2020-04-02 17:09:57 +0200

    Add files via upload

---
+++ s2m8uf2e2_diaz/blogexamen/index.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>carsandfilms</title>
+  <link rel="stylesheet" href="css/960.css">
+  <link rel="stylesheet" href="css/estil.css">
+  <link rel="stylesheet" href="css/reset.css">
+</head>
+<body>
+  <div class="container_12 contenidor">
+    <div id="cap">
+      <div id="logocap" class="grid_6 push_3">
+        <h1>cars</h1>
+        <h2>and</h2>
+        <h1>films</h1>
+      </div>
+      <div id="ani" class="grid_8 push_2">
+        <img src="css/img/ani/ghost.png">
+      </div>
+    </div>
+
+    <div class="clear"></div>
+    <div id="cos">
+      <div id="fila1">
+        <div id="ficha1" class="grid_4 ficha">
+          <img src="css/img/small/small_pulp.jpg">
+          <h2>Pulp Fiction</h2>
+        </div>
+        <div id="ficha2" class="grid_4 ficha">
+          <img src="css/img/small/small_taxi.jpg">
+          <h2>Taxidriver</h2>
+        </div>
+        <div id="ficha3" class="grid_4 ficha">
+          <img src="css/img/small/small_bond.jpg">
+          <h2>Goldfinger</h2>
+        </div>
+      </div>
+
+      <div class="clear"></div>
+      <div id="fila2">
+        <div id="" class="grid_4 ficha">
+          <img src="css/img/small/small_pulp.jpg">
+          <h2>Ghost Busters</h2>
+        </div>
+        <div id="" class="grid_4 ficha">
+          <img src="css/img/small/small_taxi.jpg">
+          <h2>Mr. Bean</h2>
+        </div>
+        <div id="" class="grid_4 ficha">
+          <img src="css/img/small/small_bond.jpg">
+          <h2>Back to the Future</h2>
+        </div>
+      </div>
+
+      <div class="clear"></div>
+      <div id="fila3">
+        <div id="" class="grid_4 ficha">
+          <img src="css/img/small/small_pulp.jpg">
+          <h2>Knight Rider</h2>
+        </div>
+        <div id="" class="grid_4 ficha">
+          <img src="css/img/small/small_taxi.jpg">
+          <h2>The A-Team</h2>
+        </div>
+        <div id="" class="grid_4 ficha">
+          <img src="css/img/small/small_bond.jpg">
+          <h2>Breaking Bad</h2>
+        </div>
+      </div>
+    </div>
+    <div class="clear"></div>
+    <div id="peu" class="grid_12">
+      <img src="css/img/logo_carsandfilms.png">
+    </div>
+</div>
+</body>
+</html>
